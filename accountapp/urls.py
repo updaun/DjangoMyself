b2cf6bd16457bbe57c0067f6fa94fec262a3363a
updaun <updauney@daum.net>
2021-07-13 10:19:22 +0900

    debug refresh error

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from accountapp.views import hello_world
 
-app_names = 'accountapp'
+app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
