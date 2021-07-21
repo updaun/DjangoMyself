3feae54ba6334f033752fb5e75a7079b4bbeb9ed
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-07-21 16:19:40 +0900

    apply decorator func

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,32 +1,31 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 
-
+@login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        #return HttpResponse('Hello_world')
-        if request.method == 'POST':
-            temp = request.POST.get("input_text")
+    if request.method == 'POST':
+        temp = request.POST.get("input_text")
 
-            model_instance = NewModel()
-            model_instance.text = temp
-            model_instance.save()
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()
 
-            return HttpResponseRedirect(reverse("accountapp:hello_world"))
-        else:
-            data_list = NewModel.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
+        return HttpResponseRedirect(reverse("accountapp:hello_world"))
     else:
-        return HttpResponseRedirect(reverse("accountapp:login"))
+        data_list = NewModel.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
+
 
 class AccountCreateView(CreateView):
     model = User
@@ -39,6 +38,8 @@ class AccountDetailView(DetailView):
     context_object_name = "target_user"
     template_name = "accountapp/detail.html"
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
@@ -46,32 +47,10 @@ class AccountUpdateView(UpdateView):
     context_object_name = "target_user"
     template_name = "accountapp/update.html"
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseRedirect(reverse("accountapp:login"))
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseRedirect(reverse("accountapp:login"))
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     success_url = reverse_lazy("accountapp:hello_world")
     context_object_name = "target_user"
     template_name = "accountapp/delete.html"
-
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseRedirect(reverse("accountapp:login"))
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseRedirect(reverse("accountapp:login"))
