74e5527a9f4eaa89289be8dd3488cc3e62143f9c
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-08-25 14:47:18 +0900

    add like messages

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -19,12 +20,16 @@ class LikeArticleView(RedirectView):
         like_record = LikeRecord.objects.filter(user=user,
                                                 article=article)
         if like_record.exists():
+            # 좋아요 X
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
         else:
+            # 좋아요 O
             LikeRecord(user=user, article=article).save()
 
         article.like += 1
         article.save()
+        messages.add_message(request, messages.SUCCESS, '사용자가 이 글을 좋아합니다.')
 
         return super().get(request, *args, **kwargs)
 
