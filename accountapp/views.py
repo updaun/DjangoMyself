6acbf5a53ff449ab3d3030987418dd0be0a85079
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-08-27 09:24:59 +0900

    medium editer thema & apply article update

--- accountapp/views.py
+++ accountapp/views.py
@@ -34,7 +34,7 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy("accountapp:hello_world")
+    success_url = reverse_lazy("articleapp:list")
     template_name = "accountapp/create.html"
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
@@ -65,6 +65,6 @@ class AccountUpdateView(UpdateView):
 @method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
-    success_url = reverse_lazy("accountapp:hello_world")
+    success_url = reverse_lazy("articleapp:list")
     context_object_name = "target_user"
     template_name = "accountapp/delete.html"
