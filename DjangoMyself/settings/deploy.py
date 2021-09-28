e312b7bbbbb79bae7f169b40c8563c9571640224
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-09-28 10:20:49 +0900

    deploy secret setting update

--- DjangoMyself/settings/deploy.py
+++ DjangoMyself/settings/deploy.py
@@ -11,10 +11,10 @@ def read_secret(secret_name):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ...
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = read_secret('DJANGO_SECRET_KEY')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
